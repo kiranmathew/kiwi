3f374b43bfe7bacb1abfd80c7bc4d87bc9f3580e
ZunZiAc <rajkirantmkr@gmail.com>
2020-10-16 11:40:18 +1300

    rff

--- Controllers/AccomodationController.cs
+++ Controllers/AccomodationController.cs
@@ -1,6 +1,9 @@
 ﻿using kiwi.Models;
+using kiwi.Models.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,18 +28,83 @@ namespace kiwi.Controllers
         // GET: Default/Create
         public ActionResult Create()
         {
-            return View();
+            AccomodationViewModels accVM = new AccomodationViewModels();
+            accVM.AccomType = db.AccomodationTypes.ToList();
+           
+            return View(accVM);
         }
 
         // POST: Default/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(AccomodationViewModels acc ,HttpPostedFileBase file)
         {
             try
             {
                 // TODO: Add insert logic here
 
-                return RedirectToAction("Index");
+                Accomodation AccModel = new Accomodation();
+
+                bool Success = false;
+
+                if (ModelState.IsValid)
+                {
+                    string path = "";
+                    
+                    if (file != null)
+                    {
+                            acc.AccomodationImage = file.FileName;
+                            path = Server.MapPath("~\\Content\\Uploads\\");
+
+                            file.SaveAs(path + file.FileName);
+
+                            System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+                            foreach (FileInfo files in di.GetFiles())
+                            {
+                                if (files.Name != acc.AccomodationImage)
+                                {
+                                    files.Delete();
+                                }
+
+                            }
+                        
+                    }
+
+                    AccModel.AccomodationTypeId = acc.AccomodationTypeId;
+                    AccModel.AccomodationName = acc.AccomodationName;
+                    AccModel.AccomodationLink = acc.AccomodationLink;
+                    AccModel.AccomodationPhone = acc.AccomodationPhone;
+                    AccModel.AccomodationAddress = acc.AccomodationAddress;
+                    AccModel.AccomodationImage = path;
+                    AccModel.AccomodationDesciption = acc.AccomodationDesciption;
+                    AccModel.AccomodationEmail = acc.AccomodationEmail;
+                    AccModel.CreatedDate = DateTime.Now;
+                    AccModel.ModifiedDate = DateTime.Now;
+                    AccModel.IsDelete = false;
+                    AccModel.IsActive = false;
+
+
+                    //using (db = new kiwilandDbEntities())
+                    //{
+                    //    using (DbContextTransaction trans = db.Database.BeginTransaction())
+                    //    {
+                            db.Accomodations.Add(AccModel);
+                            int status = db.SaveChanges();
+                            if (status > 0)
+                            {
+                                Success = true;
+                                //trans.Commit();
+                            }
+                            else
+                            {
+                                //trans.Rollback();
+                            }
+
+                    //    }
+                    //}
+
+                }
+               return RedirectToAction("Index");
             }
             catch
             {
