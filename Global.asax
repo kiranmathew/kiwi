dbbf4be4ff836b8eea63850060675561eeba5d0b
ZunZiAc <rajkirantmkr@gmail.com>
2020-10-16 08:49:35 +1300

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="kiwi.MvcApplication" Language="C#" %>
---
+++ Models/ViewModels/AccomodationViewModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace kiwi.Models.ViewModels
+{
+    public class AccomodationViewModels
+    {
+        public int AccomodationId { get; set; }
+        public int AccomodationTypeId { get; set; }
+        public string AccomodationTypeName { get; set; }
+        public string AccomodationName { get; set; }
+        public string AccomodationImage { get; set; }
+        public string AccomodationAddress { get; set; }
+        public string AccomodationPhone { get; set; }
+        public string AccomodationEmail { get; set; }
+        public string AccomodationDesciption { get; set; }
+        public string AccomodationLink { get; set; }
+        public string Description { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+
+
+    }
+}
