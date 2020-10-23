6895462796dc104c5e1a023e5efea16180cd20d8
ZunZiAc <rajkirantmkr@gmail.com>
2020-10-23 22:44:48 +1300

    JOb Controller

---
+++ Models/ViewModels/JobViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace kiwi.Models.ViewModels
+{
+    public class JobViewModel
+    {
+        public int JobId { get; set; }
+        public int JobTypeId { get; set; }
+        public int PayTypeId { get; set; }
+        public string JobTitile { get; set; }
+        public string JobDescription { get; set; }
+        public string JobLocation { get; set; }
+        public Nullable<System.DateTime> JobPostDate { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+
+
+       
+        public string JobTypeName { get; set; }
+
+        public string PayType { get; set; }
+
+        public List<JobType> jobTypeList { get; set; }
+        public List<PayType> payTypeList { get; set; }
+    }
+}
